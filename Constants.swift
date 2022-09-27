b0bc3d68355729c8b446e78d88b5c7e6eaff5211
RrezonCurraj <89447238+RrezonCurraj@users.noreply.github.com>
2022-09-27 10:44:32 +0200

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  StoreGround24
+//
+//  Created by rrezon on 23.9.22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
